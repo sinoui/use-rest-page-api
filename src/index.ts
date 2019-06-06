2d83c26341844480002a616a8b8ffb6b51cdddb0
tianyanqiu <tianyanqiu@sinosoft.com.cn>
2019-06-06 09:11:48 +0800

    chore: 调整项目导出，添加类型声明的导出

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 import useRestPageApi from './useRestPageApi';
+import { Options, PageInfo, PageResponse } from './types';
 
 export default useRestPageApi;
+
+export { Options, PageInfo, PageResponse };
